c7f7e9f7ebf6bde6e662bf5b893c262c75758b9f
Alex Tran <alextran@everledger.io>
2019-02-18 12:06:32 +1100

    reindent lines

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-    "github.com/hyperledger/fabric/core/chaincode/shim/ext/cid"
+	"github.com/hyperledger/fabric/core/chaincode/shim/ext/cid"
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
@@ -17,50 +17,50 @@ func (t *CC) Init(stub shim.ChaincodeStubInterface) pb.Response {
 }
 
 func (t *CC) testGetID(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-
-
+	
+	
 	invokeID, err := cid.GetID(stub)
 	if err != nil {
-        fmt.Println("unable to get ID ", err)
+		fmt.Println("unable to get ID ", err)
 		return shim.Error(err.Error())
 	}
-    fmt.Println("invokeID = ", invokeID)
-
-
-
+	fmt.Println("invokeID = ", invokeID)
+	
+	
+	
 	mspID, err := cid.GetMSPID(stub)
 	if err != nil {
-        fmt.Println("unable to get MSPID ", err)
+		fmt.Println("unable to get MSPID ", err)
 		return shim.Error(err.Error())
 	}
-    fmt.Println("MSP ID = ", mspID)
-
-
-    attrValue, found, err := cid.GetAttributeValue(stub, "attr1")
+	fmt.Println("MSP ID = ", mspID)
+	
+	
+	attrValue, found, err := cid.GetAttributeValue(stub, "attr1")
 	if err != nil {
-        fmt.Println("unable to get attribute value ", err)
+		fmt.Println("unable to get attribute value ", err)
 		return shim.Error(err.Error())
 	}
-
-    if found {
-        fmt.Println("attribute value found; value = ", attrValue)
-    } else {
-        fmt.Println("attribute value not found")
-    }
-
-	return shim.Success(nil)
+	
+	if found {
+		fmt.Println("attribute value found; value = ", attrValue)
+		} else {
+			fmt.Println("attribute value not found")
+		}
+		
+		return shim.Success(nil)
 }
-
+	
 func (t *CC) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	function, args := stub.GetFunctionAndParameters()
 	fmt.Printf("Chaincode Invoke; function='%s'\n", function)
 	if function == "testGetID" {
-        return t.testGetID(stub, args)
-    }
-
+		return t.testGetID(stub, args)
+	}
+	
 	return shim.Error("Invalid invoke function name")
 }
-
+	
 func main() {
 	err := shim.Start(new(CC))
 	if err != nil {
